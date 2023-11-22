93c375673d8103eda760d42c52bdd4d86037b96a
Carina De souza <carinadesouza@Carinas-Air.mshome.net>
2023-11-22 10:21:24 +0530

    compelete project

--- app/components/dashboard/rooms/page.tsx
+++ app/components/dashboard/rooms/page.tsx
@@ -1,30 +1,43 @@
+"use client"
+import { Button, Form, Input } from 'antd';
+import React, { useState } from 'react';
+
+type LayoutType = Parameters<typeof Form>[0]['layout'];
 
 export default function RoomsPage() {
-  return (
-    <div className="bg-white h-[90vh] overflow-auto  p-10">
-      changes from room
-      <h1 className="text-3xl">from rohit laptop</h1>
-      <h1 className="text-3xl">from carina laptop</h1>
+  const [form] = Form.useForm();
+  const [formLayout, setFormLayout] = useState<LayoutType>('inline');
 
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <h1>Rooms details</h1>
-      <div className="h-[100rem]"></div>
+  const onFormLayoutChange = ({ layout }: { layout: LayoutType }) => {
+    setFormLayout(layout);
+  };
+
+  const formItemLayout =
+    formLayout === 'horizontal' ? { labelCol: { span: 4 }, wrapperCol: { span: 14 } } : null;
+
+  const buttonItemLayout =
+    formLayout === 'horizontal' ? { wrapperCol: { span: 14, offset: 4 } } : null;
+
+  return (
+    <div className="bg-white h-[90vh] overflow-auto p-10">
+      <Form
+        {...formItemLayout}
+        layout={formLayout}
+        form={form}
+        initialValues={{ layout: formLayout }}
+        onValuesChange={onFormLayoutChange}
+        style={{ maxWidth: formLayout === 'inline' ? 'none' : 600 }}
+      >
+        <Form.Item label="Field A" name="fieldA">
+          <Input placeholder="Input placeholder" />
+        </Form.Item>
+        <Form.Item label="Field B" name="fieldB">
+          <Input placeholder="Input placeholder" />
+        </Form.Item>
+        <Form.Item {...buttonItemLayout}>
+          <Button type="primary">Submit</Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
